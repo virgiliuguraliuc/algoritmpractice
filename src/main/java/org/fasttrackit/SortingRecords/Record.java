8e0e3867141d75253148867f914b8ed04c314a6e
virgiliu guraliuc <virgiliuguraliuc@gmail.com>
2019-07-17 20:14:56 +0300

    sorting records

---
+++ src/main/java/org/fasttrackit/SortingRecords/Record.java
@@ -0,0 +1,23 @@
+package org.fasttrackit.SortingRecords;
+
+public class Record {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
